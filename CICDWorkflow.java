238af86876567f8e5fe3f21e0a2d561eb5e39eb6
anirbangupta7044 <anirban.gupta@hcl.com>
2021-12-10 19:28:45 +0000

    2nd Commit: 1st commit from remote: Dockerfile created for new image

---
+++ CICDWorkflow.java
@@ -0,0 +1,5 @@
+class CICD {
+    public static void main(String[] args) {
+      System.out.println("This is a simple java app created using Docker");
+    }
+  }
---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM java:8
+COPY . /var/www/java
+WORKDIR /var/www/java
+RUN javac CICDWorkflow.java
+CMD ["java", "CICD"]
+
