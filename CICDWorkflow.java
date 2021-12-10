7a3a7f6ab25cf6a4738848ff40f28af0dab5ffc8
anirbangupta7044 <anirban.gupta@hcl.com>
2021-12-10 21:24:19 +0000

    Line added to Java File to test Automation

--- CICDWorkflow.java
+++ CICDWorkflow.java
@@ -1,5 +1,6 @@
 class CICD {
     public static void main(String[] args) {
       System.out.println("This is a simple java app created using Docker");
+System.out.println("Adding a line to test automation");
     }
   }
